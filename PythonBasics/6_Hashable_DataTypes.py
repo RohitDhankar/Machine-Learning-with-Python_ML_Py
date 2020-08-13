b03efd684dca93d576d43ac3c362a88705738288
RohitDhankar <dhankar.rohit@gmail.com>
2020-08-13 21:50:48 +0530

    6_Hashable_DataTypes__eq__

--- PythonBasics/6_Hashable_DataTypes.py
+++ PythonBasics/6_Hashable_DataTypes.py
@@ -1,3 +1,7 @@
+#https://en.wikipedia.org/wiki/Hash_function
+#https://en.wikipedia.org/wiki/Immutable_object
+
+
 # Source == https://docs.python.org/3.8/glossary.html
 
 """
@@ -28,6 +32,18 @@ And can be compared to other objects (it needs an __eq__() or __cmp__() method).
       objects by packing them into a tuple and hashing the tuple.
 """
 
+"""
+If a class does not define an __eq__() method it should not define a __hash__()
+ operation either;
+  if it defines __eq__() but not __hash__(), its instances will not be usable
+   as items in hashable collections. 
+   If a class defines mutable objects and implements an __eq__() method,
+    it should not implement __hash__(), 
+    since the implementation of hashable collections requires that a key’s
+     hash value is immutable (if the object’s hash value changes,
+      it will be in the wrong hash bucket).
+"""
+
 urTuple = (1,2,(44,55))
 print(hash(urTuple)) #-2674867466080592608
 
@@ -54,5 +70,25 @@ Traceback (most recent call last):
     print(hash(urTupleDict)) #
 TypeError: unhashable type: 'dict'
 """
+#
+print(urTupleDict.__eq__(urTupleList)) # False
+print(urTupleDict.__eq__(urTupleDict)) # True 
+print(urTupleDict.__eq__(urTuple)) # False
+"""
+# Source == https://docs.python.org/3.8/reference/datamodel.html#object.__eq__
+These are the so-called “rich comparison” methods. 
+The correspondence between operator symbols and method names is as follows:
+x<y calls x.__lt__(y), x<=y calls x.__le__(y), x==y calls x.__eq__(y), 
+x!=y calls x.__ne__(y), x>y calls x.__gt__(y), and x>=y calls x.__ge__(y).
+"""
+#
+#print(urTupleDict.__cmp__(urTupleDict)) #Uncomment
+# AttributeError: 'tuple' object has no attribute '__cmp__'
 
+#
+urDict = {"A":44,"B":55}
+urDict1 = {"A":45,"B":56}
+#print(urDict1.__cmp__(urDict)) #Uncomment
+#AttributeError: 'dict' object has no attribute '__cmp__' 
+#
 
