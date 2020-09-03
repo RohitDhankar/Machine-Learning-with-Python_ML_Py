5434b473b1adc77ce001ce690bb988e8b4e44480
RohitDhankar <dhankar.rohit@gmail.com>
2020-09-03 15:16:13 +0530

    STATIC_TYPED better than DYANMIC_TYPED for Execution Speed

--- PythonBasics/temp_scripts/dynaTyp.py
+++ PythonBasics/temp_scripts/dynaTyp.py
@@ -1,3 +1,12 @@
+"""
+To understand how we can speed up Python code - we need to understand the speed-up advantages we get from STATIC TYPING
+In a language which is STATIC TYPED in-place of DYANMIC TYPED - at runtime there need not be TYPE CHECKS and thus the code is excuted faster
+
+CyThon - https://cython.readthedocs.io/en/latest/src/userguide/language_basics.html
+
+"""
+
+
 # Python is Strongly Typed and also Dynamically Typed
 
 # Whats a STRONGLY Typed Language - 
@@ -16,11 +25,42 @@ c = a + b
 print(type(c)) # <class 'float'>
 
 # As seen above we can ADD an INT to a FLOAT this is an example of OPERATOR OVERLOADING - 
-# thus also shows that Python is not very highly - Strictly Typed . 
+# OPERATOR OVERLOADING is DIFFERENT from -  implicit conversion - of the TYPE
 # CANT do this in  C++ 
+# Also in Python cant add a STR to a FLOAT 
 
+#strFloat = "str" + a #TypeError: can only concatenate str (not "float") to str
+#
 
+# Source_SO - https://stackoverflow.com/a/11328980/4928635
 
+def to_number(x):
+    """Try to convert function argument to float-type object."""
+    try: 
+        return float(x) 
+    except (TypeError, ValueError): 
+        return 0 
+
+class Foo:
+    def __init__(self, number): 
+        print("--number--from__init__-",number)
+        self.number = number
+        print("--self.number--from__init__-",self.number)
+
+    def __add__(self, other):
+        print("----other----",other)
+        print("--self.number--from__add__-",self.number)
+        return self.number + to_number(other)
+
+obj_ClsFoo = Foo(42)
+#print(obj_ClsFoo) # An Object of the Class Foo == <__main__.Foo object at 0x7fd18ef24a10>
+
+someStr = obj_ClsFoo + "str1"
+print(someStr) # 42
+#
+str_to_num = obj_ClsFoo + "2"
+print(str_to_num) # 44.0
+#
 
 
 
@@ -30,10 +70,17 @@ print(type(c)) # <class 'float'>
 
 concatStr = "str ..1 " + "str ..2 "
 print(concatStr)
-ls1 = ["ele1 " , "ele12 "]
-ls2 = ["ele3 " , "ele14 "]
+ls1 = ["ele1 " , "ele2 "]
+ls2 = ["ele3 " , "ele4 "]
 print(ls1+ls2)
 # 
+"""
+To understand how we can speed up Python code - we need to understand the speed-up advantages we get from STATIC TYPING
+In a language which is STATIC TYPED in-place of DYANMIC TYPED - at runtime there need not be TYPE CHECKS and thus the code is excuted faster
+
+CyThon - https://cython.readthedocs.io/en/latest/src/userguide/language_basics.html
+
+"""
 
 
 
