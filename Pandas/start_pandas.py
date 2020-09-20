26a7d954d9e39ef0ccd9f88ec514452f06f375d6
RohitDhankar <dhankar.rohit@gmail.com>
2020-09-20 20:13:15 +0530

    pandas_start_options_Config

---
+++ Pandas/start_pandas.py
@@ -0,0 +1,29 @@
+# Source - Real Python blog - 
+
+def start():
+    """
+    # Don't wrap to multiple pages
+    # Max length of printed sequence
+    # Controls SettingWithCopyWarning
+    """
+    options = {
+        'display': {
+                'max_columns': None,
+                'max_colwidth': 25,
+                'expand_frame_repr': False,
+                'max_rows': 14,
+                'max_seq_items': 50,
+                'precision': 4,
+                'show_dimensions': False
+                    },
+        'mode': {
+            'chained_assignment': None
+                }
+            }
+for category, option in options.items():
+    for op, value in option.items():
+        pd.set_option(f'{category}.{op}', value)
+
+if __name__ == '__main__':
+    start()
+    del start # Clean up namespace in the interpreter
