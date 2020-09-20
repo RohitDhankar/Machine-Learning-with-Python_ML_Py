fd5b2a08dc25b42cb39289b42c80e4bea9155dc8
RohitDhankar <dhankar.rohit@gmail.com>
2020-09-20 22:06:51 +0530

    dask

--- Pandas/start_pandas.py
+++ Pandas/start_pandas.py
@@ -2,6 +2,13 @@
 # Official PANDAS - https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.set_option.html
 # https://github.com/pydata/bottleneck
 
+"""
+Error - Check --- 
+>>> pd.get_option('display.max_rows')
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+AttributeError: module 'pandas' has no attribute 'get_option'
+"""
 def start():
     """
     # Don't wrap to multiple pages
@@ -22,10 +29,11 @@ def start():
             'chained_assignment': None
                 }
             }
-for category, option in options.items():
-    for op, value in option.items():
-        pd.set_option(f'{category}.{op}', value)
 
-if __name__ == '__main__':
-    start()
-    del start # Clean up namespace in the interpreter
+    for category, option in options.items():
+        for op, value in option.items():
+            pd.set_option(f'{category}.{op}', value)
+
+    if __name__ == '__main__':
+        start()
+        del start # Clean up namespace in the interpreter
