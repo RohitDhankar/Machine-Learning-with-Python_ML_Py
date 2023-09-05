74dab9738e5126146f601429f2bc63cd5114ab36
RohitDhankar <dhankar.rohit@gmail.com>
2023-09-06 00:39:51 +0530

    start--skew--ocr--opencv--1

--- src/ocr_opencv/src/test_ocr_1.py
+++ src/ocr_opencv/src/test_ocr_1.py
@@ -5,6 +5,7 @@
 from PIL import Image
 import pytesseract
 import argparse , cv2 , os 
+import numpy as np
 
 def init_ocr():
     """
@@ -46,24 +47,90 @@ def init_ocr():
 
 
 
-def correct_skew(image):
+def correct_skew(image_init,img_name):
     """
     https://arxiv.org/pdf/1109.3317.pdf
     https://arxiv.org/pdf/1801.00824.pdf
     https://arxiv.org/pdf/2305.14672.pdf
 
+    getRotationMatrix2D --> 
+            Parameters
+            center	Center of the rotation in the source image.
+            angle	Rotation angle in degrees. Positive values mean counter-clockwise rotation (the coordinate origin is assumed to be the top-left corner).
+            scale	Isotropic scale factor. 
+
     """
-    # convert the image to grayscale and flip the foreground
-    # and background to ensure foreground is now "white" and
+    # convert the image to grayscale and flip the foreground     # and background to ensure foreground is now "white" and
     # the background is "black"
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(image_init, cv2.COLOR_BGR2GRAY)
     gray = cv2.bitwise_not(gray)
     # threshold the image, setting all foreground pixels to 255 and all background pixels to 0
     img_thresh = cv2.threshold(gray, 0, 255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-    cv2.imwrite("./data_dir/output_dir/img_skew/skw_1.png", img_thresh)
+    
+    # grab the (x, y) coordinates of all pixel values that     # are greater than zero, then use these coordinates to
+    # compute a rotated bounding box that contains all     # coordinates
+    coords = np.column_stack(np.where(img_thresh > 0))
+    print("---np.shape-Coordinates---",np.shape(coords))
+    print("---np.shape-Coordinates---coords.dtype--",coords.shape, coords.dtype)
+    
+    #imageIntegral = cv2.integral(src=coords)
+    # division results in values ranging from 0.0 to 1.0
+    # type is floating point array (float64)
+    # presentable = imageIntegral / imageIntegral.max()
+
+    # cv2.imshow("imageIntegral", presentable)
+    # cv2.waitKey(5000) # 5 Secs
+    # cv2.destroyWindow('imageIntegral')
+
+
+    # image_coords = coords.astype(np.uint8)
+    # cv2.imshow('test_coords', image_coords)
+    # cv2.waitKey(5000) # 5 Secs
+    # cv2.destroyWindow('test_coords')
+    angle = cv2.minAreaRect(coords)[-1]
+    print("--Coordinates--Angle-",angle)
+
+    # the `cv2.minAreaRect` function returns values in the     # range [-90, 0); as the rectangle rotates clockwise the
+    # returned angle trends to 0 -- in this special case we     # need to add 90 degrees to the angle
+    if angle < -45:
+        angle = -(90 + angle)
+    # otherwise, just take the inverse of the angle to make     # it positive
+    else:
+        angle = -angle
+
+    # rotate the image_init to deskew it
+    (h, w) = image_init.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(image_init, M, (w, h),flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    # cv2.imshow('show_rotated', rotated)
+    # cv2.waitKey(5000) # 5 Secs
+    # cv2.destroyWindow('show_rotated')
+
+    # draw the correction angle on the image so we can validate it
+    cv2.putText(rotated, "Angle: {:.2f} degrees".format(angle),	(10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+    # show the output image
+    print("[INFO] angle: {:.3f}".format(angle))
+    cv2.imshow("image_init", image_init)
+    cv2.imshow("show_rotated", rotated)
+    cv2.waitKey(5000) # 5 Secs
+    cv2.destroyWindow('show_rotated')
+    cv2.destroyWindow('image_init')
+    cv2.imwrite("./data_dir/output_dir/img_skew/rotated_"+str(img_name)+"_.png", rotated)
+
+
 
 
 if __name__ == "__main__":
-    path_img = "./data_dir/input_dir/img_ocr_7.png"
-    image = cv2.imread(path_img)
-    correct_skew(image)     
+    path_img1 = "./data_dir/input_dir/img_skw_1.png"
+    path_img2 = "./data_dir/input_dir/img_skw_2.png"
+    path_img3 = "./data_dir/input_dir/img_skw_3.png"
+
+    ls_imgs = [path_img1,path_img2,path_img3]
+
+    for iter_img in range(len(ls_imgs)):
+        img_name = str(ls_imgs[iter_img]).rsplit("/",1)[1]
+        img_name = str(img_name).rsplit(".png",1)[0]
+        print("--name ---",img_name)
+        image = cv2.imread(ls_imgs[iter_img])
+        correct_skew(image,img_name)     
