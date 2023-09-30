54f863d4475056c7c7281f6961b460189fc1ca99
RohitDhankar <dhankar.rohit@gmail.com>
2023-09-30 18:44:38 +0530

    sebastianraschka--ml-book-code

---
+++ src/rasbt_ml_book_code/rasbt_ch03.py
@@ -0,0 +1,23 @@
+## SOURCE -- https://github.com/rasbt/machine-learning-book/blob/main/ch03/ch03.py
+
+from sklearn import datasets
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+# Load Iris from - sklearn -- as a <class 'sklearn.utils._bunch.Bunch'>
+# https://scikit-learn.org/stable/modules/generated/sklearn.utils.Bunch.html#sklearn.utils.Bunch.keys
+
+iris = datasets.load_iris()
+
+X = iris.data[:, [2, 3]]
+y = iris.target
+print("--[INFO]--type(iris)--",type(iris)) #--type(iris)-- <class 'sklearn.utils._bunch.Bunch'>
+print("--[INFO]--iris.keys()--",iris.keys()) # - dict_keys(['data', 'target', 'frame', 'target_names', 'DESCR', 'feature_names', 'filename', 'data_module'])
+ 
+print("--[INFO]--type(iris.data)--",type(iris.data)) #-- <class 'numpy.ndarray'>
+print("--[INFO]--type(iris.target)--",type(iris.target)) #-- <class 'numpy.ndarray'>
+print('Class labels:', np.unique(y))
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=y)
+
